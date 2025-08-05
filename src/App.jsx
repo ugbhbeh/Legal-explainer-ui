3029d6fd37ed396da7108a4bd1c3ee49fd706f7c
root <187004982+ugbhbeh@users.noreply.github.com>
2025-08-05 05:19:28 +0000

    app.jsx done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import { useState, useEffect } from 'react';
 
 function App() {
-  
-  return (
-    <>
-     
-    </>
+    const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
+  useEffect(() => {
+    const handleStorage = () => {
+      setIsAuthenticated(!!localStorage.getItem('token'));
+    };
+    window.addEventListener('storage', handleStorage);
+    return () => window.removeEventListener('storage', handleStorage);
+  }, []);
+ 
+ 
+    return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home isAuthenticated={isAuthenticated} />} />
+        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!isAuthenticated ? <Signup /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
   )
+  
 }
 
 export default App
